1f4a09a3d1bec9c81ee3d39e8a981df48d247112
girogiacomo <39419444+girogiacomo@users.noreply.github.com>
2023-01-21 23:24:54 +0100

    Fixed typos

--- Wireguard/genpeer.sh
+++ Wireguard/genpeer.sh
@@ -4,8 +4,8 @@
 # This code is provided under the terms of the GNU General Public License version 2 only (GPL-2.0)
 
 # This is a crude beta version of a simple script that generates peer configuration files
-# for wireguard tunnel[s] in /etc/wireguard/peers/ given the self exolanatory parameters
-# run it without parameters to get the correct syntax.
+# for wireguard tunnel[s] in /etc/wireguard/peers/ given the self explanatory parameters.
+# Run it without parameters to get the correct syntax.
 # The IP addresses are substituted by letters for obvious reasons but it should be easy 
 # to replace them with the correct ones.
 
